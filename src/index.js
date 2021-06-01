990736b858ed40cada031b5e84c4dd87b04fbb35
gihong-park <dev.gihong2012@gmail.com>
2021-06-02 00:25:55 +0900

    first commit

---
+++ src/index.js
@@ -0,0 +1,33 @@
+import AFRAME from 'aframe';
+import 'aframe-physics-system';
+
+let scene;
+
+AFRAME.registerComponent('move-circle', {
+  tick(currentTime) {
+    this.el.object3D.position.x = (Math.sin(currentTime / 250) * 1.2);
+    this.el.object3D.position.z = (Math.cos(currentTime / 250) * 1.2) - 4;
+
+    this.el.object3D.rotation.x = (Math.sin(currentTime / 400) * 3.14);
+    this.el.object3D.rotation.z = (Math.cos(currentTime / 400) * 3.14);
+  }
+});
+
+let createSphere = () => {
+  scene = document.querySelector('a-entity');
+
+  for (let i = 0; i < 100; i++) {
+    const sphere = document.createElement('a-sphere');
+    const x = (Math.random() - 0.5) * 1.5;
+    const y = Math.random() + 1;
+    const z = ((Math.random() - 0.5) * 1.5) - 4;
+    sphere.setAttribute('position', '' + x + ' ' + y + ' ' + z);
+    sphere.setAttribute('radius', 0.25);
+    sphere.setAttribute('dynamic-body', 'shape:auto');
+    sphere.setAttribute('shadow', '');
+    sphere.setAttribute('color', '#EF2D5E');
+    scene.appendChild(sphere);
+  }
+}
+
+window.addEventListener('click', createSphere);
---
+++ webpack.config.js
@@ -0,0 +1,48 @@
+var path = require('path');
+
+module.exports = {
+  devtool: 'source-map',
+  entry: './src/index.js',
+  mode: 'development',
+
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'app.js',
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+    fallback: {
+      "fs": false,
+      "tls": false,
+      "net": false,
+      "path": false,
+      "zlib": false,
+      "http": false,
+      "https": false,
+      "stream": false,
+      "crypto": false,
+      "cannon": false,
+    }
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.(wasm)$/,
+        type: "javascript/auto",
+        use: {
+          loader: "file-loader",
+          options: {
+            outputPath: "assets/wasm", //set this whatever path you desire
+            name: "[name]-[hash].[ext]"
+          }
+        },
+      },
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|build)/,
+        loader: 'babel-loader',
+      },
+    ],
+  },
+};
